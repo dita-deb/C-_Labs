89663f219492fd5a2df4222234a5a558eb7b77ff
Anindita Deb <153967732+dita-deb@users.noreply.github.com>
2024-04-11 23:51:44 -0400

    Create Lab2B.cpp

---
+++ Lab2/Lab2B.cpp
@@ -0,0 +1,24 @@
+/*
+Class: CSE 1321L
+Section: WE1 C++
+Term: Summer 2021
+Instructor: Mokter Hossain, Ph.D
+Name: Anindita Deb
+Lab#: Lab2B
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string s1 = "___*___";
+    string s2 = "__*_*__";
+    string s3 = "_*_*_*_";
+    string s4 = "*_*_*_*";
+    string s5 = "__*_*__";
+    string s6 = "___*___";
+
+    cout << s1 << "\n" << s2 << "\n" << s3 << "\n" << s4 << "\n" << s5 << "\n" << s6 << endl;
+}
