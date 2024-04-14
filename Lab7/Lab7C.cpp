d2f3370d883b2c91e6c68fc98fa11393a6a74f66
Anindita Deb <153967732+dita-deb@users.noreply.github.com>
2024-04-14 00:43:42 -0400

    Create Lab7C.cpp

---
+++ Lab7/Lab7C.cpp
@@ -0,0 +1,44 @@
+/*
+Class: CSE 1321L
+Section: WE1 C++
+Term: Spring 2021
+Instructor: Samyuktha,Devi
+Name: Anindita Deb
+Lab#: Lab7C
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int s; //size
+	cout << "Enter the size of the arrays: ";
+	cin >> s;
+
+	int array1['s'];
+	int array2['s'];
+
+	//loop for the array variables for array1 and array2
+	for (int i = 0; i < s; i++)
+	{
+		cout << "Enter array 1 slot " << i << ": "; //taking input in array1;
+		cin >> array1[i];
+		cout << "Enter array 2 slot " << i << ": "; //taking input in array 2;
+		cin >> array2[i];
+	}
+
+	//now to compare each array by checking array variables if they are equal
+	int check=1; 
+	for (int i = 0; i < s; i++)
+	{
+		if (array1[i] != array2[i])
+		{
+			check = 0;
+			break; //break if any element is not identical
+		}
+	}
+	if (check == 0)
+		cout << "The arrays are not identical";
+	else
+		cout << "The arrays are identical";
+}
