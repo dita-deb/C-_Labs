136a4adf07243ff121aa297af13db1231e0f05f7
Anindita Deb <153967732+dita-deb@users.noreply.github.com>
2024-04-22 14:32:51 -0400

    Create Lab9B.cpp

---
+++ Lab9/Lab9B.cpp
@@ -0,0 +1,31 @@
+/*
+Class: CSE 1321L
+Section: WE1 C++
+Term: Summer 2021
+Instructor: Mokter Hossain
+Name: Anindita Deb
+Lab#: 9B
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string pw;
+	bool length;
+	bool uppercase;
+	bool digit;
+	length = uppercase = digit = false;
+
+	cout << "Enter a password: ";
+	getline(cin, pw);
+
+	if (length >= 8) {
+		length = true;
+		if () {
+
+		}
+	}
+}
