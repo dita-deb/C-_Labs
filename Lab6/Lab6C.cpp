fe41a21bb36dd60e1342c342c9e1a2db90385a03
Anindita Deb <153967732+dita-deb@users.noreply.github.com>
2024-04-22 14:42:46 -0400

    Update Lab6C.cpp

--- Lab6/Lab6C.cpp
+++ Lab6/Lab6C.cpp
@@ -1,39 +1,52 @@
 /*
 Class: CSE 1321L
 Section: WE1 C++
-Term: Spring 2021
-Instructor: Samyuktha,Devi
+Term: Summer 2021
+Instructor: Mokter Hossain
 Name: Anindita Deb
-Lab#: Lab6A
+Lab#: Lab6C
 */
+
+
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int start;
-	int end;
-	cout << "Enter a starting number: ";
-	cin >> start;
-	cout << "Enter an ending number: ";
-	cin >> end;
+	int num;
+	char choice;
+	int bal = 1000;
+	int newBal, amount;
 
-	int sum = 0;
 
-	if (start < end) {
-		for (int i = start; i <= end; i++) {
-			if (i % 2 != 0) {
-				sum += i;
-			}
-		}
-	}if (start > end) {
-		for (int i = start; i >= end; i--) {
-			if (i % 2 != 0) {
-				sum += i;
-			}
+	cout << "Welcome!\nYou have $1000 in your account.\n";
+	cout << "\nMenu \n0-Make a deposit \n1-Make a withdrawl \n2-Display account value";
+	do {
+		cout << "\nPlease make a selection: ";
+		cin >> num;
+		switch (num) {
+		case 0:
+			cout << "How much would you like to deposit?: ";
+			cin >> amount;
+			newBal = bal + amount;
+			cout << "Your current balance is $" << newBal << "\n";
+			cout << "Would you like to return to the main menu(Y / N)?: ";
+			cin >> choice;
+		case 1:
+			cout << "How much would you like to withdraw?: ";
+			cin >> amount;
+			newBal = bal - amount;
+			cout << "Your current balance is $" << newBal << "\n";
+			cout << "Would you like to return to the main menu (Y/N)?: ";
+			cin >> choice;
+		case 2:
+			cout << "Your current balance is $" << newBal << "\n";
+			cout << "Would you like to return to the main menu (Y/N)?: ";
+			cin >> choice;
+			while (choice == 'N' || choice == 'n');
+				cout << "Thank you for banking with us!";
+			
 		}
 	}
-	cout << "Sum of odds is: " << sum << end;;
-
-}  
+}
